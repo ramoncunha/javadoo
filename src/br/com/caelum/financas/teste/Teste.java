e9a9129585443ebccd682b17929b427a8dd8a510
ramoncunha <ramon.network@hotmail.com>
2018-06-11 00:15:43 -0300

    Adicionando JPAUtil

--- src/br/com/caelum/financas/teste/Teste.java
+++ src/br/com/caelum/financas/teste/Teste.java
@@ -1,10 +1,9 @@
 package br.com.caelum.financas.teste;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class Teste {
 
@@ -16,15 +15,13 @@ public class Teste {
 		conta.setAgencia("123");
 		conta.setNumero("456");
 		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
-		EntityManager em = emf.createEntityManager();
+		EntityManager em = new JPAUtil().getEntityManager();
 		
 		em.getTransaction().begin();
 		em.persist(conta);
 		em.getTransaction().commit();
 		
 		em.close();
-		emf.close();
 
 	}
 
