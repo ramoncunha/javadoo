341f1c4732ac03cffcd8b7d931999a2628072c1c
ramoncunha <ramon.network@hotmail.com>
2018-06-11 20:40:51 -0300

    Colocando métodos estáticos

--- src/br/com/caelum/financas/util/JPAUtil.java
+++ src/br/com/caelum/financas/util/JPAUtil.java
@@ -4,11 +4,19 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class JPAUtil {
+public final class JPAUtil {
+	private JPAUtil(){
+		
+	}
 	
 	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
 	
-	public EntityManager getEntityManager () {
+	public static EntityManager getEntityManager () {
 		return emf.createEntityManager();
 	}
 }
+/*// JPQL JPA Query Language
+Query<Conta> query = em.createQuery("select c from Conta c  join c.transacoes t where t.valor > 10000").;
+query.setParameter(":cpf", "11.1111.1111-00");
+
+List<Conta> contas - query.list();*/
