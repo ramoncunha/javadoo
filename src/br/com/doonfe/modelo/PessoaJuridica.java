4865c405dc99d53f51e61355c77c4ec3c7622ced
ramoncunha <ramon.network@hotmail.com>
2018-06-18 20:41:23 -0300

    corrigindo classe pessoa juridica

--- src/br/com/doonfe/modelo/PessoaJuridica.java
+++ src/br/com/doonfe/modelo/PessoaJuridica.java
@@ -1,11 +1,8 @@
 package br.com.doonfe.modelo;
 
 import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public class PessoaJuridica extends Pessoa {
 
 	private String cnpj;
