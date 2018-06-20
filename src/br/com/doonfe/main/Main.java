e60ae2f6bbb9b4836f842436388e19c5f6a7082b
ramoncunha <ramon.network@hotmail.com>
2018-06-20 13:06:54 -0300

    Adicionando Barra de Ferramentas

--- src/br/com/doonfe/main/Main.java
+++ src/br/com/doonfe/main/Main.java
@@ -1,12 +1,16 @@
 package br.com.doonfe.main;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JToolBar;
 
 public class Main {
 
@@ -41,8 +45,27 @@ public class Main {
 		menubar.add(fileSobre);
 		/* ****************FIM JMENU BAR**************** */
 		
+		/* ****************Criando JToolBar**************** */
+		JButton btnNovo = new JButton();
+		btnNovo.setIcon(new ImageIcon(btnNovo.getClass().getResource("/images/novo.png")));
+		JButton btnEditar = new JButton();
+		btnEditar.setIcon(new ImageIcon(btnEditar.getClass().getResource("/images/editar.png")));
+		JButton btnExcluir = new JButton();
+		btnExcluir.setIcon(new ImageIcon(btnExcluir.getClass().getResource("/images/excluir.png")));
+		JButton btnDetalhes = new JButton();
+		btnDetalhes.setIcon(new ImageIcon(btnDetalhes.getClass().getResource("/images/detalhes.png")));
+		
+		JToolBar toolbar = new JToolBar("Barra de Ferramentas");
+		toolbar.add(btnNovo);
+		toolbar.add(btnEditar);
+		toolbar.add(btnExcluir);
+		toolbar.add(btnDetalhes);
+		/* ****************FIM JTOOLBAR**************** */
+		
+		
 		JFrame frame = new JFrame("Tela Inicial");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(toolbar, BorderLayout.PAGE_START);
 		frame.setJMenuBar(menubar);
 		frame.setSize(800, 600);
 		frame.setVisible(true);
