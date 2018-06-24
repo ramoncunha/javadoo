ca90bb04441e7f6632d9f622f6bbdc5df0fd33c9
ramoncunha <ramon.network@hotmail.com>
2018-06-24 16:57:49 -0300

    Adicionando Emitente/Destinatario

--- src/br/com/doonfe/telas/TelaCadastro.java
+++ src/br/com/doonfe/telas/TelaCadastro.java
@@ -1,5 +1,8 @@
 package br.com.doonfe.telas;
 
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,27 +14,70 @@ public class TelaCadastro {
 
 	public static void main(String[] args) {
 		
+		/* Campos Formulário Nota Fiscal */
 		JTextField campoNNota = new JTextField(10);
 		JTextField campoModelo = new JTextField(10);
+		campoModelo.setText("Modelo 1-A");
+		campoModelo.setEditable(false);
 		JTextField campoNatureza = new JTextField(10);
+		campoNatureza.setText("Venda");
+		campoNatureza.setEditable(false);
 		JTextField campoDtOperacao = new JTextField(10);
 		JTextField campoDtEmissao = new JTextField(10);
 		
-		JPanel formulario = new JPanel();
-		formulario.add(new JLabel("N Nota"));
-		formulario.add(campoNNota);
-		formulario.add(new JLabel("Modelo"));
-		formulario.add(campoModelo);
-		formulario.add(new JLabel("Natureza"));
-		formulario.add(campoNatureza);
-		formulario.add(new JLabel("Data da Operação"));
-		formulario.add(campoDtOperacao);
-		formulario.add(new JLabel("Data da Emissão"));
-		formulario.add(campoDtEmissao);
+		/* Campos Formulário Pessoa */
+		JTextField campoDocumento1 = new JTextField(15);
+		JTextField campoNome1 = new JTextField(15);
+		JTextField campoInscricao1 = new JTextField(15);
+		JTextField campoEstado1 = new JTextField(5);
+		
+		JTextField campoDocumento2 = new JTextField(15);
+		JTextField campoNome2 = new JTextField(15);
+		JTextField campoInscricao2 = new JTextField(15);
+		JTextField campoEstado2 = new JTextField(5);
+		
+		/* JPane Formulário Nota Fiscal */
+		JPanel formularioNF = new JPanel();
+		//formularioNF.setLayout(new BoxLayout(formularioNF, BoxLayout.Y_AXIS));
+		formularioNF.add(new JLabel("N Nota"));
+		formularioNF.add(campoNNota);
+		formularioNF.add(new JLabel("Modelo"));
+		formularioNF.add(campoModelo);
+		formularioNF.add(new JLabel("Natureza"));
+		formularioNF.add(campoNatureza);
+		formularioNF.add(new JLabel("Data da Operação"));
+		formularioNF.add(campoDtOperacao);
+		formularioNF.add(new JLabel("Data da Emissão"));
+		formularioNF.add(campoDtEmissao);
+		
+		/* JPane Formulário Pessoa */
+		JPanel formularioPessoa = new JPanel();
+		//LayoutManager layoutPessoa = new FlowLayout(FlowLayout.RIGHT);
+		//formularioPessoa.setLayout(layoutPessoa);
+		formularioPessoa.add(new JLabel("EMITENTE"));
+		formularioPessoa.add(new JLabel("CNPJ/CPF"));
+		formularioPessoa.add(campoDocumento1);
+		formularioPessoa.add(new JLabel("Razão Social/Nome"));
+		formularioPessoa.add(campoNome1);
+		formularioPessoa.add(new JLabel("Inscrição Estadual"));
+		formularioPessoa.add(campoInscricao1);
+		formularioPessoa.add(new JLabel("Estado"));
+		formularioPessoa.add(campoEstado1);
+		
+		formularioPessoa.add(new JLabel("DESTINATARIO"));
+		formularioPessoa.add(new JLabel("CNPJ/CPF"));
+		formularioPessoa.add(campoDocumento2);
+		formularioPessoa.add(new JLabel("Razão Social/Nome"));
+		formularioPessoa.add(campoNome2);
+		formularioPessoa.add(new JLabel("Inscrição Estadual"));
+		formularioPessoa.add(campoInscricao2);
+		formularioPessoa.add(new JLabel("Estado"));
+		formularioPessoa.add(campoEstado2);
 		
 		JPanel pai = new JPanel();
-		pai.add(formulario);
-		pai.setLayout(new BoxLayout(pai, BoxLayout.Y_AXIS));
+		pai.add(formularioNF);
+		pai.add(formularioPessoa);
+		pai.setLayout(new BoxLayout(pai, BoxLayout.X_AXIS));
 		
 		JFrame janela = new JFrame();
 		janela.setTitle("Cadastro");
@@ -39,8 +85,5 @@ public class TelaCadastro {
 		janela.add(pai);
 		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		janela.setVisible(true);
-		
-		
 	}
-
 }
