6e5f29f84ac61e94c72ca9cf2a45af79b6fe3fde
ramoncunha <ramon.network@hotmail.com>
2018-06-26 15:37:41 -0300

    Persist Nota Fiscal

--- src/br/com/doonfe/dao/NotaFiscalDAO.java
+++ src/br/com/doonfe/dao/NotaFiscalDAO.java
@@ -1,10 +1,22 @@
 package br.com.doonfe.dao;
 
+import javax.persistence.EntityManager;
+
 import br.com.doonfe.modelo.NotaFiscal;
+import br.com.doonfe.util.JPAUtil;
 
 public class NotaFiscalDAO {
 
 	public void salvarNotaFiscal(NotaFiscal notaFiscal){
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		em.getTransaction().begin();
+		
+		em.persist(notaFiscal);
+		
+		em.getTransaction().commit();
+		em.close();
 		
 	}
 	
