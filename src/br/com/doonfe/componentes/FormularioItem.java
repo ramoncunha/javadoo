f1a1df585e12e567873d84dd6e57cfd2bcf947f4
ramoncunha <ramon.network@hotmail.com>
2018-06-26 14:35:13 -0300

    Especializando form item

---
+++ src/br/com/doonfe/componentes/FormularioItem.java
@@ -0,0 +1,58 @@
+package br.com.doonfe.componentes;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class FormularioItem {
+	private JTextField campoCodigo = new JTextField(15);
+	private JTextField campoDescricao = new JTextField(15);
+	private JTextField campoPreco = new JTextField(15);
+	private JTextField campoQtd = new JTextField(15);
+	
+	public JTextField getCampoCodigo() {
+		return campoCodigo;
+	}
+	public void setCampoCodigo(JTextField campoCodigo) {
+		this.campoCodigo = campoCodigo;
+	}
+	public JTextField getCampoDescricao() {
+		return campoDescricao;
+	}
+	public void setCampoDescricao(JTextField campoDescricao) {
+		this.campoDescricao = campoDescricao;
+	}
+	public JTextField getCampoPreco() {
+		return campoPreco;
+	}
+	public void setCampoPreco(JTextField campoPreco) {
+		this.campoPreco = campoPreco;
+	}
+	public JTextField getCampoQtd() {
+		return campoQtd;
+	}
+	public void setCampoQtd(JTextField campoQtd) {
+		this.campoQtd = campoQtd;
+	}
+	
+	public JPanel buildFormularioItem() {
+		/* JPanel Item */
+		JPanel camposItem = new JPanel();
+		camposItem.setLayout(new BoxLayout(camposItem, BoxLayout.Y_AXIS));
+		camposItem.setBorder(BorderFactory.createTitledBorder("ITEM"));
+		camposItem.add(new JLabel("Código"));
+		camposItem.add(campoCodigo);
+		camposItem.add(new JLabel("Descrição"));
+		camposItem.add(campoDescricao);
+		camposItem.add(new JLabel("Preço"));
+		camposItem.add(campoPreco);
+		camposItem.add(new JLabel("Quantidade"));
+		camposItem.add(campoQtd);
+		JButton salvarItem = new JButton("Inserir");
+		camposItem.add(salvarItem);
+		return camposItem;
+	}
+}
