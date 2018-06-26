32a0a0d4f1e7e179ff9fdf6834f459fea5bf711b
ramoncunha <ramon.network@hotmail.com>
2018-06-25 22:58:19 -0300

    Classe MenuBar

---
+++ src/br/com/doonfe/componentes/MenuBar.java
@@ -0,0 +1,42 @@
+package br.com.doonfe.componentes;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MenuBar {
+	
+	public JMenuBar build() {
+		JMenuItem eMenuSair = new JMenuItem("Sair");
+		eMenuSair.setMnemonic(KeyEvent.VK_S);
+		eMenuSair.setToolTipText("Sair da Aplicação");
+		eMenuSair.addActionListener((ActionEvent event) -> {
+		    System.exit(0);
+		});
+		// Opção incluir Nota Fiscal
+		JMenuItem eMenuIncluirNota = new JMenuItem("Incluir Nota Fiscal");
+		eMenuIncluirNota.setMnemonic(KeyEvent.VK_N);
+		eMenuIncluirNota.setToolTipText("Incluir nova Nota Fiscal");
+		// Aba Arquivo
+		JMenu fileArquivo = new JMenu("Arquivo");
+		fileArquivo.setMnemonic(KeyEvent.VK_F);
+		fileArquivo.add(eMenuSair);
+		// Aba Cadastro
+		JMenu fileCadastro = new JMenu("Cadastro");
+		fileCadastro.setMnemonic(KeyEvent.VK_I);
+		fileCadastro.add(eMenuIncluirNota);
+		// Aba Sobre
+		JMenu fileSobre = new JMenu("Sobre");
+		fileSobre.setMnemonic(KeyEvent.VK_S);
+		// MENU BAR
+		JMenuBar menubar = new JMenuBar();
+		menubar.add(fileArquivo);
+		menubar.add(fileCadastro);
+		menubar.add(fileSobre);
+		
+		return menubar;
+	}
+}
