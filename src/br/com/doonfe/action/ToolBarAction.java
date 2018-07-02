9c5e008e13f94262a0ccbbe541fcb97cf495626f
ramoncunha <ramon.network@hotmail.com>
2018-07-02 15:14:48 -0300

    metodo para selecionar id

--- src/br/com/doonfe/action/ToolBarAction.java
+++ src/br/com/doonfe/action/ToolBarAction.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import br.com.doonfe.telas.TelaCadastro;
+
 public class ToolBarAction {
 	
 	public ActionListener excluirLinha(JTable tabela) {
@@ -17,7 +19,6 @@ public class ToolBarAction {
 					int linha = tabela.getSelectedRow();
 					if(linha != -1) {
 						((DefaultTableModel) tabela.getModel()).removeRow(linha);
-						//System.out.println("Conteúdo -> " + tabela.getModel().getValueAt(linha, 0).toString());
 					}
 				} catch(ArrayIndexOutOfBoundsException e) {
 					System.out.println(e);
@@ -26,6 +27,30 @@ public class ToolBarAction {
 		};
 		
 		return ExcluirLinha;
+	}
+	
+	public ActionListener editarLinha(JTable tabela) {
+		
+		ActionListener editarLinha = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					int linha = tabela.getSelectedRow();
+					if(linha != -1) {
+						int valorId = (int) tabela.getModel().getValueAt(linha, 0);
+						
+						TelaCadastro TelaCadastro = new TelaCadastro();
+						TelaCadastro.setEditarNf(valorId);
+						TelaCadastro.render();
+						
+						System.out.println("Conteúdo -> " + valorId);
+					}
+				} catch(ArrayIndexOutOfBoundsException er) {
+					System.out.println(er);
+				}				
+			}
+		};
+		return editarLinha;
 	}
 	
 }
