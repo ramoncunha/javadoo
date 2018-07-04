86c961282a8bbf58875f8b7d81662990a37e8715
ramoncunha <ramon.network@hotmail.com>
2018-07-04 11:06:48 -0300

    Retirando jpql

--- src/br/com/doonfe/dao/NotaFiscalDAO.java
+++ src/br/com/doonfe/dao/NotaFiscalDAO.java
@@ -1,7 +1,6 @@
 package br.com.doonfe.dao;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 import br.com.doonfe.modelo.NotaFiscal;
 import br.com.doonfe.util.JPAUtil;
@@ -27,9 +26,7 @@ public class NotaFiscalDAO {
 		
 		em.getTransaction().begin();
 		
-		String jpql = "select nf from NotaFiscal nf join fetch nf.itens where nf.id = " + id;
-		Query query = em.createQuery(jpql);
-		NotaFiscal registroNf = (NotaFiscal) query.getSingleResult();
+		NotaFiscal registroNf = em.find(NotaFiscal.class, id);
 		
 		em.getTransaction().commit();
 		em.close();
